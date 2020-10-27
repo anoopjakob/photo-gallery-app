16f3fa0f7ed96124039e57d157238278fceccf76
anoopjakob <anoopjacob9@gmail.com>
2020-10-28 04:18:25 +0530

    editing title

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -4,7 +4,7 @@ const Title = () => (
 	<div className="title">
 		<h1>Photo Gallery</h1>
 		<h2>Your Pictures</h2>
-		<p>Upload and see your Pictures Here.</p>
+		<p>Upload and see your Pictures Here. Yaayyy</p>
 	</div>
 );
 
